516fd6c49025b1d9e51bd5e67e7f4b74d6f56618
ivanvl01 <1v4nvl@gmail.com>
2021-11-19 13:21:34 +0200

    first commit

---
+++ src/Incidents/Clip.js
@@ -0,0 +1,18 @@
+import MotorCortex from '@donkeyclip/motorcortex';
+import lottieWeb from 'lottie-web'
+
+export default class MyClip extends MotorCortex.BrowserClip{
+
+    onAfterRender(){
+        const animation = lottieWeb.loadAnimation({
+            container: this.context.rootElement,
+            path: this.attrs.path,
+            renderer: this.attrs.renderer || "svg",
+            loop: this.attrs.autoloop || false,
+            autoplay: this.attrs.autoloop || false,
+            name: "Gabishlagya",
+        });
+
+        this.setCustomEntity("lottie", animation);
+    }
+}
